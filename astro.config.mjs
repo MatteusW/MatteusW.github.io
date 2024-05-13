51ba9bb97322465aed4aaf1e84bbadf34fd42e53
M1nxy <m1nxd3v@gmail.com>
2024-05-13 22:57:55 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://matteus.art',
   integrations: [mdx(), sitemap(), tailwind(), svelte()]
 });
